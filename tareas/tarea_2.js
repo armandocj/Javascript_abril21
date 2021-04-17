a763fa5d8da05f897f4e65ccc0e2c324b422404c
armandocj <armando.celada1@gmail.com>
2021-04-17 09:19:36 -0500

    actualizar tarea 2

--- tareas/tarea_2.js
+++ tareas/tarea_2.js
@@ -1,9 +1,9 @@
-// tarea 2: factorial de un numnero N
+// tarea 2: factorial de un numero N
 
-var numeroInicial = 5;
+var numeroFactorial = 5;
 var factorial = 1;
-for (var i = 1; i <= numeroInicial; i++) {
+for (var i = 1; i <= numeroFactorial; i++) {
     factorial *= i;
 }
 
-console.log('El factorial de: ' + numeroInicial + ' es: ' + factorial);
+console.log('El factorial de: ' + numeroFactorial + ' es: ' + factorial);
