97b9a2c1d36562954b57b14309a3f3bcfb092d12
armandocj <armando.celada1@gmail.com>
2021-04-17 13:27:21 -0500

    actualizando las tareas 1, 2, 3 y 4

--- tareas/tarea_4.js
+++ tareas/tarea_4.js
@@ -1,1 +1,25 @@
 // TArea 4: Crear objetos casa, dentro objetos personas
+console.info('### Tarea 4: Crear objetos casa y que tenga dentro objetos personas \n')
+
+function persona (nombre, genero) {
+    this.nombre = nombre
+    this.genero = genero
+}
+
+function casa (calle, numero, personas) {
+    this.calle = calle
+    this.numero = numero
+    this.personas = personas
+}
+
+var familia = {}
+familia.papa = new persona('Armando', 'Hombre')
+familia['mama'] = new persona('Diana', 'Mujer')
+familia.hija = new persona('Valeria', 'Mujer')
+
+var miCasa = new casa('Bronce', 49, familia);
+
+console.log(miCasa)
+
+
+
